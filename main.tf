e7227e7b6f9391c680c15340776b363373550377
Mralbert93 <matthew.albert@hashicorp.com>
2023-09-22 08:05:43 -0400

    First push

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "tfe" {
+    token = var.token
+}
+
+data "tfe_slug" "test_slug_policyset" {
+source_path = "policy-sets/qaent-dev-azure-sentinel-policy-set"
+}
+
+resource "tfe_policy_set" "test_slug_based_policy_set" {
+
+name = "test-policy-set"
+
+description = "A test policy set"
+organization = "hashicorp-matthew"
+
+slug = data.tfe_slug.test_slug_policyset
+
+}
